473c167464c083ca3e7bfd4bafe7dc4a3d8dff6a
Gabriel Fernandez Xavier <gabrielps3psn@hotmail.com>
2017-12-16 04:26:47 -0200

    Comitao

--- server/server.js
+++ server/server.js
@@ -3,29 +3,29 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.render('/dist/index.html');
+  res.sendFile('/index.html');
 });
 
-io.on('connection', function (socket) {
+// io.on('connection', function (socket) {
 
-  console.log('a user connected');
+//   console.log('a user connected');
 
-  socket.on('newMessage', function (msg) {
-    var data = new Date();
-    var hora = data.getHours() + ":" + data.getMinutes();
-    var dados = {
-        msg,
-        hora
-    }
-    io.emit('newMessage', dados)
-    console.log(dados)
-  });
+//   socket.on('newMessage', function (msg) {
+//     var data = new Date();
+//     var hora = data.getHours() + ":" + data.getMinutes();
+//     var dados = {
+//         msg,
+//         hora
+//     }
+//     io.emit('newMessage', dados)
+//     console.log(dados)
+//   });
   
-  socket.on('newUser', function (user) {
-    io.emit('newUser', user)
-    console.log(user)
-  });
-});
+//   socket.on('newUser', function (user) {
+//     io.emit('newUser', user)
+//     console.log(user)
+//   });
+// });
 
 http.listen(3111, function () {
   console.log('listening on *:3111');
