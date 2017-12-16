f02adae5cd5d761a152857ad4f7cdec4521f9fcc
Gabriel Fernandez Xavier <gabrielps3psn@hotmail.com>
2017-12-16 16:20:41 -0200

    Alivio

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var cors = require('cors')
+
+app.use(cors());
 
 app.use(express.static(__dirname + '/public/static'));
 
