03f70bf261fe07eb3ea4080e602e03023855d2f3
Gabriel Fernandez Xavier <gabrielps3psn@hotmail.com>
2017-12-16 05:00:47 -0200

    agrvai

---
+++ server.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendFile('/dist/index.html');
+});
+
+// io.on('connection', function (socket) {
+
+//   console.log('a user connected');
+
+//   socket.on('newMessage', function (msg) {
+//     var data = new Date();
+//     var hora = data.getHours() + ":" + data.getMinutes();
+//     var dados = {
+//         msg,
+//         hora
+//     }
+//     io.emit('newMessage', dados)
+//     console.log(dados)
+//   });
+  
+//   socket.on('newUser', function (user) {
+//     io.emit('newUser', user)
+//     console.log(user)
+//   });
+// });
+
+http.listen(3111, function () {
+  console.log('listening on *:3111');
+});
