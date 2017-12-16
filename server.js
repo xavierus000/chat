572161224067a087e89d06f392cfadcde253bf1c
Gabriel Fernandez Xavier <gabrielps3psn@hotmail.com>
2017-12-16 15:17:31 -0200

    AGORA SIM

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const app = express();
 
 app.use(express.static(__dirname + '/dist/static'));
 
