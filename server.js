d0d76818b6bc1d5aa3ca339858706f87dd0e6203
Gabriel Fernandez Xavier <gabrielps3psn@hotmail.com>
2017-12-16 15:59:02 -0200

    cansei

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
@@ -31,3 +31,5 @@ io.on('connection', function (socket) {
 });
 
 http.listen(process.env.PORT || 3111)
+
+console.log(__dirname + '/dist')
